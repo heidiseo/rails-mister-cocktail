0476d408a88a8d14f913ca358ffee5526aa161e3
Heidi Seo <heidiseo1@gmail.com>
2019-05-14 15:59:24 +0100

    adding cocktails and doeses models, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
+
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_list = open(url).read
+drinks = JSON.parse(drinks_list)
+drinks["drinks"].each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
+Cocktail.create(name: "Mojito", ingredient: "rum")
+
